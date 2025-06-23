cccdb7cc807ccb824a57b2ab867618f564be3a5a
Endrure <115525009+Endrure@users.noreply.github.com>
2025-06-23 04:34:25 +0300

    Update run_noshadow.py
    final version1

--- run_noshadow.py
+++ run_noshadow.py
@@ -3,17 +3,21 @@
 from time import time
 from common.tk_drawer import TkDrawer
 from noshadow.polyedr import Polyedr
-
+import ans
 
 tk = TkDrawer()
 try:
-    for name in ["ccc", "cube", "box", "king", "cow"]:
+    for name in ["custom_no"]:
+        ans.ans = 0
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
         Polyedr(f"data/{name}.geom").draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        print("=============================================================")
+        print(ans.ans, "= сумма площадей")
+        print("=============================================================")
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
