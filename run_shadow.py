87a8baeecd8700decb95ce227340c10aa69904cc
Endrure <115525009+Endrure@users.noreply.github.com>
2025-06-23 15:47:26 +0300

    Update run_shadow.py
    теперь выводит ответ

--- run_shadow.py
+++ run_shadow.py
@@ -3,17 +3,29 @@
 from time import time
 from common.tk_drawer import TkDrawer
 from shadow.polyedr import Polyedr
-
+import ans
 
 tk = TkDrawer()
 try:
-    for name in ["ccc", "cube", "box", "king", "cow"]:
+    for name in ["ccc", "custom_yes", "cube", "box", "king", "cow"]:
+        ans.ans = 0
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
         Polyedr(f"data/{name}.geom").draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        print("=============================================================")
+        print(ans.ans, "= сумма площадей")
+        print("=============================================================")
+        tk.canvas.create_text(
+            890, 10,
+            text=str(ans.ans),
+            anchor='ne',
+            font=('Arial', 14),
+            fill='red'
+        )
+        tk.root.update()
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
